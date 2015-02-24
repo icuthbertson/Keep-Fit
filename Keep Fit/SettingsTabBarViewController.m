79c17a444d848174b038899bcbd156bae7672b3e
Iain Michael Cuthbertson <iain.cuthbertson.2013@uni.strath.ac.uk>
2015-02-24 15:31:11 +0000

    committing before starting to add the testing mode section available through settings

---
+++ Keep Fit/SettingsTabBarViewController.m
@@ -0,0 +1,37 @@
+//
+//  SettingsTabBarViewController.m
+//  Keep Fit
+//
+//  Created by Iain Cuthbertson on 24/02/2015.
+//  Copyright (c) 2015 Iain Cuthbertson. All rights reserved.
+//
+
+#import "SettingsTabBarViewController.h"
+
+@interface SettingsTabBarViewController ()
+
+@end
+
+@implementation SettingsTabBarViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
